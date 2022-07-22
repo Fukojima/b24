304fe827469046536563fe6f829f1e6ae9112e71
leonardo.fenix <leonardofenix10@gmail.com>
2022-07-22 14:26:52 -0300

    .

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance  = axios.create({
   //baseURL: "https://back-aps-tascom.herokuapp.com/api/",
-  baseURL: "http://https://tms-back.herokuapp.com/api"
+  baseURL: "https://tms-back.herokuapp.com/api"
 });
 export default instance;
