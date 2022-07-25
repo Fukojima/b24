a85c3534e4d22e82d66d5bfd8dc4b3fc8a687a1d
Leonardo Fenix <leonardofenix10@gmail.com>
2022-07-25 16:38:43 -0300

    Update api.js

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance  = axios.create({
-  //baseURL: "https://back-aps-tascom.herokuapp.com/api/",
-  baseURL: "http://localhost:3333/api"
+  baseURL: "https://back-aps-tascom.herokuapp.com/api/",
+  //baseURL: "http://localhost:3333/api"
 });
 export default instance;
